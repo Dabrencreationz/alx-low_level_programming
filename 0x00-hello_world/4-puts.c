de99485cbb037cf7f514ae216bd2d21ac609c7bc
Brenda <brenvaltessy@yahoo.com>
2022-09-08 08:17:24 -0700

    Hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+
+
+
 /**
  *
  *  * main - Entry point
@@ -12,8 +15,13 @@
  *      * Return: Always 0 (Success)
  *
  *      */
+
+
+
 int main(void)
+
 {
+
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
