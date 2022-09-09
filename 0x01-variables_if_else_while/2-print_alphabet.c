ea2e0da21b01bee20d946798699500feb284ef3d
Brenda <brenvaltessy@yahoo.com>
2022-09-09 10:12:37 -0700

    Alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
  *
  * Description: print all aplhabet letters
  *
  * Return: Always 0 (Success) 
-
 */
 
 int main(void)
-
 {
 		char ch = 'a';
 
