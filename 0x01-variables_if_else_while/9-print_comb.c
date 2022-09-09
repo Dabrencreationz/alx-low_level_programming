b1e0b8b21e1cd3a52ee7fcf7466390c1ea71c9c9
Brenda <brenvaltessy@yahoo.com>
2022-09-09 14:57:09 -0700

    Ps for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,59 +1,31 @@
 #include<stdio.h>
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
+ * Description: print 0, 1, - 9
  *
- *   *
- *
- *    * Description: print 0, 1, - 9
- *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- *      */
-
-
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
-
 {
+	int digit = 0;
 
-		int digit = 0;
+	while (digit <= 9)
 
-
-
-			while (digit <= 9)
-
-					{
-
-								putchar(digit + 48);
-
-
-
-										if (digit != 9)
-
-													{
-
-																	putchar(',');
-
-																				putchar(' ');
-
-																						}
-
-
-
+	{
+		putchar(digit + 48);
+		if (digit != 9)
+												{
+			putchar(',');
+			putchar(' ');
+												}
 												++digit;
+											}
 
-													}
+	putchar('\n');
 
-				putchar('\n');
-
-
-
-					return (0);
+	return (0);
 
 }
