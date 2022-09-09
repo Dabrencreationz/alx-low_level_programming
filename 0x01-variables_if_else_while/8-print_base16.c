0a270d0ab52e5cdd4655102a97c0d2c62e625a00
Brenda <brenvaltessy@yahoo.com>
2022-09-09 14:27:26 -0700

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,53 +1,28 @@
 #include<stdio.h>
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
+ * Description: print numbers of base16 in lowercase
  *
- *   *
- *
- *    * Description: print numbers of base16 in lowercase
- *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- *      */
-
-
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
-
 {
+	int digit = 48; /*48; decimal rep of 0*/
 
-		int digit = 48; /*48; decimal rep of 0*/
+	while (digit <= 102) /*102; decimal rep of f*/
 
+	{
+		putchar(digit);
 
+		if (digit == 57)
+												digit += 39;
+		++digit;
 
-			while (digit <= 102) /*102; decimal rep of f*/
+	}
+		putchar('\n');
 
-					{
-
-								putchar(digit);
-
-
-
-										/* after 9 we jump till 96; `*/
-
-										if (digit == 57)
-
-														digit += 39;
-
-												++digit;
-
-													}
-
-				putchar('\n');
-
-
-
-					return (0);
-
+		return (0);
 }
