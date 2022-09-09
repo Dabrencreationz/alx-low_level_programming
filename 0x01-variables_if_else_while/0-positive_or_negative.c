d59b578884ae57da8cea470d905e21828d57578e
Brenda <brenvaltessy@yahoo.com>
2022-09-09 06:49:08 -0700

    Variables if, else and while

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdtime.h>
+#include <stdio.h>
+
+/**main - Entry point
+ * Description - print whether the number stored in the variable n is positive or negative.
+ * return: (0) success
+ *
+int main(void)
+{
+/** my code*/
+if(n>0)
+	printf("%; positve\n", n);
+else if (n==0)
+	printf("%; zero\n", n);
+else if (n<0)
+	print("%; negative\n", n)n;
+	return (0);
+	}
+
