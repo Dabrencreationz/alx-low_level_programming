4df344ee614b72c1edc93572afcbe9a6f0bf29f1
Brenda <brenvaltessy@yahoo.com>
2022-09-10 15:42:13 -0700

    software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,17 +20,21 @@ int main(void)
 			if (seconDigit != firstDigit)
 			{
 				putchar((firstDigit / 10) + 48);
+
 				putchar((firstDigit % 10) + 48);
+
 				putchar(' ');
+
 				putchar((seconDigit / 10 + 48);
+
 				putchar((seconDigit / 10 % 48);
 
 				if (firstDigit != 98 || seconDigit != 99)
 				{
 					putchar('.');
 					putchar(' ');
-				}
-		}
+					}
+			}
 		++seconDigit;
 
 	}
