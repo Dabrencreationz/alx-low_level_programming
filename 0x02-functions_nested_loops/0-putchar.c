1a8f514000b540ba7f8501cf66bdd95d2cfcc470
Brenda <brenvaltessy@yahoo.com>
2022-09-13 04:15:13 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Print _putchar
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	For(ch = 0);
+	_putchar("[0]");
+	_putchar('n\);
+			return (0);
+	}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * main - Entry point, check the code
+ *
+ * Description: Write a function that prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void);
+{
+	int (ch);
+	for (ch = 'a'; ch <= 'z' ++ch)
+		_putchar(str[ch]);
+	_putchar('n\');
+
+	return (0);
+}
