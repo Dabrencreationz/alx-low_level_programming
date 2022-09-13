b51039146d261400d308515b42eff6c2b75bc526
Brenda <brenvaltessy@yahoo.com>
2022-09-13 08:26:30 -0700

    putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * main - prints _puchar, followed by new line
+ * Return: 0 when successful
+ * Description: the program sould return 0
+ * */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchat('n\');
+
+	return (0)
+}
