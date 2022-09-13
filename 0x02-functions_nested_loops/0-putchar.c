4b7c91f6150cce85e231a0f0960505d1384679a2
Brenda <brenvaltessy@yahoo.com>
2022-09-13 06:11:23 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,13 @@
  *
  * Return: On success 1.
  *
- * On error, -1 is returned, and errno is set appropriately.
+ *         On error, -1 is returned, and errno is set appropriately.
  *
  */
 
 int _putchar(char c)
 
 {
-
-		return (write(1, &c, 1));
+	return (write(1, &c, 1));
 
 }
