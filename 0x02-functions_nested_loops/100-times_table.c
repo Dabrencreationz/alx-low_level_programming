66a7480270915d18d8b44efbe61b505a2c88f8d7
Brenda <brenvaltessy@yahoo.com>
2022-09-14 10:28:01 -0700

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,42 +1,40 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the ne times table, starting with 0
- * @n: number of the times table
+ * print_times_table - prints time table of n
+ * @n: takes number input
  */
 
 void print_times_table(int n)
 {
-	int i, j, k;
+	int prod, mult, num;
 
-	if (n >= 0 && n <= 15)
+	if (n <= 15 && n >= 0)
 	{
-		for (i = 0; i <= n; i++)
+		for (num = 0; num <= n; ++num)
 		{
-			for (j = 0; j <= n; j++)
+			_putchar(48);
+			for (mult = 1; mult <= n; ++mult)
 			{
-				k = j * i;
-				if (j == 0)
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 9)
+					_putchar(' ');
+				if (prod <= 99)
+					_putchar(' ');
+
+				if (prod >= 100)
 				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
+					_putchar((prod / 100) + 48);
+					_putchar((prod / 10) % 10 + 48);
+				} else if (prod <= 99 && prod >= 10)
+					_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
 			}
 			_putchar('\n');
 		}
 	}
 }
-
