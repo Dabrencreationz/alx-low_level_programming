83edaa584d44495a066ed91f351251496cf09ee3
Brenda <brenvaltessy@yahoo.com>
2022-09-14 05:29:36 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,21 +19,21 @@ void times_table(void)
 		for (c = 1; c <= 9; c++)
 		{
 			d = (r * c);
-			if ((d / 10) > 0)
-			{
-				_putchar((d / 10) + '0');
-			}
-			else
-			{
-				_putchar(' ');
-			}
-				_putchar((d % 10) + '0');
-				if (c < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				_putchar('\n');
+		if ((d / 10) > 0)
+		{
+			_putchar((d / 10) + '0');
+		}
+		else
+		{
+			_putchar(' ');
+		}
+			_putchar((d % 10) + '0');
+		if (c < 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
 		}
 	}
 }
