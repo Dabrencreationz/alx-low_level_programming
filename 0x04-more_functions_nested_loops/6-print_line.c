7ea119c2a1947d1f445fd3b1f3b8341b4e47ba51
Brenda <brenvaltessy@yahoo.com>
2022-09-15 05:45:12 -0700

    more functions, nested and loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - print a straight line
+ * @n: number of times _ character be printed
+ */
+
+void print_line(int n)
+{
+	int lnChr;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (lnChr = 1; lnChr <= n; ++lnChr)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_square - print a square using the character #
+ * @size: is the size of the square
+ * Return: Always 0 (success)
+ */
+
+void print_square(int size)
+{
+	int row, column;
+
+	for (row = 1; row <= size; ++row)
+	{
+		for (column = 1; column <= size; ++column)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
