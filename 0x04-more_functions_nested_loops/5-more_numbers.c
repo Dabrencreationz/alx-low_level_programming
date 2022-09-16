2f577b7ec7ce7e467cb9323cf7c2f8d130b315c8
Brenda <brenvaltessy@yahoo.com>
2022-09-16 03:41:45 -0700

    more functions, nested, loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,8 +15,7 @@ void more_numbers(void)
 		for (count = 0; count <= 14; ++count)
 		{
 			num = count;
-
-			if (count > 9);
+			if (count > 9)
 			{
 				_putchar(1 + 48);
 				num = count % 10;
@@ -26,3 +25,4 @@ void more_numbers(void)
 		_putchar('\n');
 	}
 }
+
