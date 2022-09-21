63d301d2ff26fd8fa650ad7e92dd790ea4a54b22
Brenda <brenvaltessy@yahoo.com>
2022-09-21 13:39:04 -0700

    poibters, arrays and strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,12 +14,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\n'; i++)
-		dest[i] = src[i];
-	while (i < n)
+	for (i = 0; i < n && *(src + i); i++)
 	{
-		dest[i] = '\0';
-		i++;
+		*(dest + i) = *(src + i);
 	}
+	for (; i < n; i++)
+	{
+		*(dest + i) = '\0';
+	}
+
 	return (dest);
 }
