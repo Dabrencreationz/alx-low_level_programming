155f0bbff2fc10735cbed18bab0ead3950a34517
Brenda <brenvaltessy@yahoo.com>
2022-09-21 14:45:45 -0700

    pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,20 +1,32 @@
-#include"main.h"
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - check the code for ALX School students
- * Return: Always 0
+ * cap_string - capitalizes all words in a string
+ * @s: string
+ * Return: address of s
  */
 
-int main(void)
+char *cap_string(char *s)
 {
-	char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes
-		\nhello world!hello-world 0123456hello world\thello world.hello world\n";
-	char *ptr;
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
 
-	ptr = cap_string(str);
-	printf("%s", ptr);
-	printf("%s", str);
-
-	return (0);
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
+			}
+		}
+		i++;
+	}
+	return (s);
 }
