febbd85ca9dd7c54dab8b2b217ac2ec439b301b3
Brenda <brenvaltessy@yahoo.com>
2022-09-26 04:13:18 -0700

    more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,13 +1,13 @@
-#ifndef M_MAIN.H
+#ifndef M_MAIN_H
 #define MAIN_H
 
 int main(void);
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-ar *_strchr(char *s, char c);
+char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-ar *_strpbrk(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
