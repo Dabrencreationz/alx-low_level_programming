24d8413ff47132b6aa499dfddb554ffc0d1c65e7
Brenda <brenvaltessy@yahoo.com>
2022-09-28 01:42:19 -0700

    Recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - A function that reverses a string
+ * @s: string
+ * Return: nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	char *s = "Colton Walker";
+
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar (*s);
+	}
+}
