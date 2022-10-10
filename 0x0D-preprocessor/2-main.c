907c0e04f045b60edfb2911c18ae74c6d6627c08
Brenda <brenvaltessy@yahoo.com>
2022-10-10 06:21:33 -0700

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,11 @@
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
- *main - Entry point
- * Description: prints the name of the file it was compiled from
- * Return: Always 0
- */
+ * main - Entry point
+ * Description: prints the name of the file it was
+ *              compiled from, followed by a new line
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
 {
