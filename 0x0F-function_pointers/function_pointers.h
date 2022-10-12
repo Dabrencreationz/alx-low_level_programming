fd223f25301db504519871c222ed01a41dddf71f
Brenda <brenvaltessy@yahoo.com>
2022-10-12 07:22:20 -0700

    funtion pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,7 +4,6 @@
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-_putchar.c
 
 /**
  * enum bool - typedef function for bool
