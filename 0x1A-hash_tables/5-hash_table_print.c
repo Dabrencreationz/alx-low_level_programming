ad84d5457fa25f7b1f685b7381dfc5f0023f4e77
Brenda <brenvaltessy@yahoo.com>
2023-01-05 11:01:22 -0800

    hash tables

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,45 +1,33 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - deletes a hash table
- * @ht: pointer to the hash table
- * Return: no return
+ * hash_table_print - print
+ * @ht: hashtable
+ * Return: hash table
  */
 
-void hash_table_delete(hash_table_t *ht)
+void hash_table_print(const hash_table_t *ht)
+
 {
 	unsigned long int i;
+	int init = 0;
 
-	hash_node_t *tmp1;
-
-	hash_node_t *tmp2;
-
-
-	if (ht == NULL)
-
+	if (!ht)
 		return;
-
-	for (i = 0; i < ht->size; i++)
-
+	printf("{");
+	for (i = 0; i <= ht->size; i++)
 	{
-		tmp1 = ht->array[i];
-
-		while ((tmp2 = tmp1) != NULL)
-
+		if (ht->array[i])
 		{
-			tmp1 = tmp1->next;
-
-			free(tmp2->key);
-
-			free(tmp2->value);
-
-			free(tmp2);
-
+			while (ht->array[i])
+			{
+				if (init != 0)
+					printf(", ");
+				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+				init = 1;
+				ht->array[i] = ht->array[i]->next;
+			}
 		}
-
 	}
-
-	free(ht->array);
-
-	free(ht);
+	printf("}\n");
 }
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,44 +1,31 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - deletes a hash table
- * @ht: pointer to the hash table
- * Return: no return
+ * hash_table_delete - delete a hash table
+ * @ht:hash table
+ * Return: success, nothing
  */
 
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
+	hash_node_t *node;
 
-	hash_node_t *tmp1;
-
-	hash_node_t *tmp2;
-
-
-	if (ht == NULL)
-
+	if (!ht || !ht->array || !ht->size)
 		return;
-
 	for (i = 0; i < ht->size; i++)
-
 	{
-		tmp1 = ht->array[i];
-
-		while ((tmp2 = tmp1) != NULL)
-
+		while (ht->array[i])
 		{
-			tmp1 = tmp1->next;
-
-			free(tmp2->key);
-
-			free(tmp2->value);
-
-			free(tmp2);
-
+			node = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = node;
 		}
 	}
-
 	free(ht->array);
-
+	ht->array = NULL;
+	ht->size = 0;
 	free(ht);
 }
